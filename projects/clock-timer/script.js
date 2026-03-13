63fc3db87945346571b9d118b65cfb02388dc05d
Khalil Khan <khlilkhn911@gmail.com>
2026-03-13 15:50:29 +0500

    :feat(clock-timer): add play, pause and reset functionality

--- projects/clock-timer/script.js
+++ projects/clock-timer/script.js
@@ -19,3 +19,58 @@ lightMode.addEventListener("click", () => {
     lightMode.classList.toggle("hidden");
     darkMode.classList.toggle("hidden");
 });
+
+startBtn.addEventListener("click", () => {
+    startBtn.classList.replace("ph-bold", "ph-fill");
+    pauseBtn.classList.replace("ph-fill", "ph-bold");
+    resetBtn.classList.replace("ph-fill", "ph-bold");
+    startTimer();
+});
+pauseBtn.addEventListener("click", () => {
+    pauseBtn.classList.replace("ph-bold", "ph-fill");
+    startBtn.classList.replace("ph-fill", "ph-bold");
+    pauseTimer();
+});
+resetBtn.addEventListener("click", () => {
+    resetBtn.classList.replace("ph-bold", "ph-fill");
+    pauseBtn.classList.replace("ph-fill", "ph-bold");
+    resetTimer();
+});
+
+
+let totalSeconds = 0;
+let intervalId = null;
+
+
+function formatTime(seconds) {
+  const h = Math.floor(seconds / 3600) % 24;
+  const m = Math.floor((seconds / 60) % 60);
+  const s = seconds % 60;
+
+  const displayHours = String(h).padStart(2, '0');
+  const displayMinutes = String(m).padStart(2, '0');
+  const displaySeconds = String(s).padStart(2, '0');
+
+  return `${displayHours}:${displayMinutes}:${displaySeconds}`;
+}
+
+function startTimer() {
+  if (intervalId !== null) return;
+
+  intervalId = setInterval(() => {
+    totalSeconds++;
+    timeDisplay.textContent = formatTime(totalSeconds);
+  }, 1000);
+}
+
+function pauseTimer() {
+  clearInterval(intervalId);
+  intervalId = null;
+}
+
+function resetTimer() {
+  clearInterval(intervalId);
+  intervalId = null;
+  totalSeconds = 0;
+  timeDisplay.textContent = "00:00:00";
+}
